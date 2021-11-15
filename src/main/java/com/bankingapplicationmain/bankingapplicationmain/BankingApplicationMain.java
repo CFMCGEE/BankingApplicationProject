5d77a7d649eb2d024650372a49ec4195af53eb05
Raul Limon-Hernandez <rlimonhernandez@wil.yearup.org>
2021-11-15 14:21:32 -0500

    send test up to github

--- src/main/java/com/bankingapplicationmain/bankingapplicationmain/BankingApplicationMain.java
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/BankingApplicationMain.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankingApplicationMain {
 
+// test run 123
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplicationMain.class, args);
 	}
