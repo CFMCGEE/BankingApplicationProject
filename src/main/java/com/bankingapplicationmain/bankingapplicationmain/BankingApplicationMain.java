61fbae07f826af3decdd42388e82ae86a857cc20
CFMCGEE <funchbusiness@gmail.com>
2021-11-15 15:00:02 -0500

    Requested packages

--- src/main/java/com/bankingapplicationmain/bankingapplicationmain/BankingApplicationMain.java
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/BankingApplicationMain.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankingApplicationMain {
 
-// test run 123
 	public static void main(String[] args) {
-		System.out.println("Hello Bank");
 		SpringApplication.run(BankingApplicationMain.class, args);
 	}
 
