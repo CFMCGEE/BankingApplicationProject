d4760a4408bcd2524fc776f6ff8622fd30eeaa3a
CFMCGEE <funchbusiness@gmail.com>
2021-11-15 16:26:42 -0500

    Main chnage

--- src/main/java/com/bankingapplicationmain/bankingapplicationmain/BankingApplicationMain.java
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/BankingApplicationMain.java
@@ -1,13 +1,31 @@
 package com.bankingapplicationmain.bankingapplicationmain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BankingApplicationMain {
 
+	private static final Logger logger = LoggerFactory.getLogger(BankingApplicationMain.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplicationMain.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> logger.info("Running application!");
 	}
 
 }
