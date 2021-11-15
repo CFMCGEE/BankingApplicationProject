778f5a48013d24316a5c90da6aed2f986409cce7
CFMCGEE <funchbusiness@gmail.com>
2021-11-15 14:05:41 -0500

    Committing skeleton project

---
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/BankingApplicationMain.java
@@ -0,0 +1,13 @@
+package com.bankingapplicationmain.bankingapplicationmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplicationMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingApplicationMain.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankingapplicationmain/bankingapplicationmain/BankingApplicationMainTests.java
@@ -0,0 +1,13 @@
+package com.bankingapplicationmain.bankingapplicationmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
