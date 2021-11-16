a023b690895e546a76b8b5d6c6b4f1f2198f2848
Boris <borisgerard333@gmail.com>
2021-11-15 23:14:35 -0500

    new commit

--- src/main/java/com/bankingapplicationmain/bankingapplicationmain/services/CustomerService.java
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/services/CustomerService.java
@@ -1,11 +1,8 @@
 package com.bankingapplicationmain.bankingapplicationmain.services;
 
-import com.bankingapplicationmain.bankingapplicationmain.models.Customer;
 import com.bankingapplicationmain.bankingapplicationmain.repositories.CustomerRepository;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -16,12 +13,4 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
-    //get customer by id
-    public ResponseEntity<Customer> getCustomerById(Long id) {
-        Optional<Customer> customer = customerRepository.findById(id);
-        if(customer.isPresent()) {
-            return ResponseEntity.ok(customer.get());
-        }
-        return ResponseEntity.notFound().build();
-    }
 }
