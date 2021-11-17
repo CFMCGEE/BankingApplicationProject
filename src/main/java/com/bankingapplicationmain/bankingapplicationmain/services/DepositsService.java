3a1a1f1cc5a007cb2cf108f4696a01e2f40078f5
Raul Limon-Hernandez <rlimonhernandez@wil.yearup.org>
2021-11-16 23:10:19 -0500

    push incomplete code to git

--- src/main/java/com/bankingapplicationmain/bankingapplicationmain/services/DepositsService.java
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/services/DepositsService.java
@@ -2,6 +2,8 @@ package com.bankingapplicationmain.bankingapplicationmain.services;
 
 import com.bankingapplicationmain.bankingapplicationmain.models.Deposits;
 import com.bankingapplicationmain.bankingapplicationmain.repositories.DepositsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,30 +11,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class DepositsService {
 
+    private static final Logger logger = LoggerFactory.getLogger(DepositsService.class);
+
     @Autowired
     private DepositsRepository depositsRepository;
+
 
     public ResponseEntity<?> getDeposits(Long accountId) {
         return ResponseEntity.ok(depositsRepository.findAll());
     }
 
     public ResponseEntity<?> getDepositById(Long id) {
+        if(depositsRepository.findById(id).isEmpty()){
+            logger.info("Deposit not found");
+         //   throw new DepositsNotFoundById(); //uncomment this when DepositsNotFoundById exception is created
+        }
         return ResponseEntity.ok(depositsRepository.findById(id));
     }
 
     //we need a post method
     public void addDeposit(Deposits deposits){
         depositsRepository.save(deposits);
+        logger.info("Deposit successfully completed");
     }
 
     //a put method as well
     public void editDeposit(Long depositId, Deposits deposits){
         depositsRepository.save(deposits);
+        logger.info("Deposit successfully Updated");
     }
 
     //delete method
     public void deleteDeposit(Long depositId){
         depositsRepository.delete(depositsRepository.getById(depositId));
+        logger.info("Deposit successfully deleted");
     }
 
 
