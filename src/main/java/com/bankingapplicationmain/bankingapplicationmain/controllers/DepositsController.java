62e425a822c57fb1b8b69ec6ec655c12c682f9b9
thatguyish <ishmell.ramirez@gmail.com>
2021-11-16 22:14:55 -0500

    fix Deposits controller

--- src/main/java/com/bankingapplicationmain/bankingapplicationmain/controllers/DepositsController.java
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/controllers/DepositsController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +16,29 @@ public class DepositsController {
     private DepositsService depositsService;
 
 
-    @RequestMapping("/accounts/{accountId}/deposits")
+    @RequestMapping(value="/accounts/{accountId}/deposits",method = RequestMethod.GET)
     public ResponseEntity<?> getDeposits(@PathVariable Long accountId) {
         return depositsService.getDeposits(accountId);
+    }
+
+    @RequestMapping(value="/deposits/{depositId}", method = RequestMethod.GET)
+    public ResponseEntity<?> getDeposit(@PathVariable Long depositId) {
+        return depositsService.getDeposits(depositId);
+    }
+
+    @RequestMapping(value="/accounts/{accountId}/deposits",method = RequestMethod.POST)
+    public ResponseEntity<?> createDeposit(@PathVariable Long accountId) {
+        return depositsService.createDeposit(accountId);
+    }
+
+    @RequestMapping(value="/deposits/{depositId}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateDeposit(@PathVariable Long depositId) {
+        return depositsService.updateDeposit(depositId);
+    }
+
+    @RequestMapping(value="/deposits/{depositId}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteDeposit(@PathVariable Long depositId) {
+        return depositsService.deleteDeposit(depositId);
     }
 
 
