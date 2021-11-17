294ff42bb18460a1e3bb8e369534124c75640a8e
Raul Limon-Hernandez <rlimonhernandez@wil.yearup.org>
2021-11-16 23:11:53 -0500

    push incomplete code to git

---
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/exceptions/DepositsNotFoundById.java
@@ -0,0 +1,11 @@
+package com.bankingapplicationmain.bankingapplicationmain.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DepositsNotFoundById extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+  public
+}
