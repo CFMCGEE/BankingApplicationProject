651b4c6fe6743777f46c6cfaa04a3f1e4ede73fd
Diego Guzman <deeaygo98@gmail.com>
2021-11-16 23:53:47 -0500

    Added Exception

---
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/exceptions/BillNotFoundException.java
@@ -0,0 +1,23 @@
+package com.bankingapplicationmain.bankingapplicationmain.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class BillNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BillNotFoundException() {
+    }
+
+    public BillNotFoundException(String message) {
+        super(message);
+    }
+
+    public BillNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+}
