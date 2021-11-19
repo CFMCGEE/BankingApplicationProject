c90aa7af5a5e5d75211ab2d1f95d63b85c19a9f8
Genbuos <83673134+Genbuos@users.noreply.github.com>
2021-11-18 23:01:50 -0500

    Mission Completed

--- src/main/java/com/bankingapplicationmain/bankingapplicationmain/exceptions/UnableToCreateBillException.java
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/exceptions/UnableToCreateBillException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UnableToCreateBillException extends RuntimeException {
-
+//adding this only from laptop
     public UnableToCreateBillException(){
 
     }
