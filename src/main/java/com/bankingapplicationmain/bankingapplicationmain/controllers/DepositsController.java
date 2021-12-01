0230299479840b5d4532330e7a837fa4cfdd91f7
CFMCGEE <funchbusiness@gmail.com>
2021-12-01 11:12:09 -0500

    CrossOriginBit

--- src/main/java/com/bankingapplicationmain/bankingapplicationmain/controllers/DepositsController.java
+++ src/main/java/com/bankingapplicationmain/bankingapplicationmain/controllers/DepositsController.java
@@ -3,9 +3,7 @@ package com.bankingapplicationmain.bankingapplicationmain.controllers;
 import com.bankingapplicationmain.bankingapplicationmain.details.success.DepositSuccessfullyCreated;
 import com.bankingapplicationmain.bankingapplicationmain.models.Deposits;
 import com.bankingapplicationmain.bankingapplicationmain.services.DepositsService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +12,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/deposits")
 public class DepositsController {
